f20f2286fe08faa76b692d111aba7a0df2b80d98
Tushar Arora <tarora238@gmail.com>
2019-03-17 12:53:47 +0530

    padding and button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, Inject, Renderer, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit, Inject, Renderer, ElementRef, ViewChild, Renderer2 } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { Subscription } from 'rxjs/Subscription';
 import 'rxjs/add/operator/filter';
 import { DOCUMENT } from '@angular/platform-browser';
 import { LocationStrategy, PlatformLocation, Location } from '@angular/common';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { HostListener } from '@angular/core';
 
 @Component({
     selector: 'app-root',
@@ -15,7 +16,7 @@ export class AppComponent implements OnInit {
     private _router: Subscription;
     @ViewChild(NavbarComponent) navbar: NavbarComponent;
 
-    constructor( private renderer: Renderer, private router: Router, @Inject(DOCUMENT)
+    constructor(private renderer: Renderer2, private router: Router, @Inject(DOCUMENT)
      private document: any, private element: ElementRef, public location: Location) {}
     ngOnInit() {
         const navbar: HTMLElement = this.element.nativeElement.children[0].children[0];
@@ -27,8 +28,9 @@ export class AppComponent implements OnInit {
             }
             this.navbar.sidebarClose();
         });
-        this.renderer.listenGlobal('window', 'scroll', (event) => {
+        this.renderer.listen('window', 'scroll', (event) => {
             const number = window.scrollY;
+            console.log(number);
             if (number > 150 || window.pageYOffset > 150) {
                 // add logic
                 navbar.classList.remove('navbar-transparent');
