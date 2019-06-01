ee7459e7e5fa0acdf4a8216472ecfe04158d5532
Tushar Arora <tarora238@gmail.com>
2019-06-01 12:39:51 +0530

    register and subscribe

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,9 @@
 export const environment = {
-  production: true,
-  url: 'https://www.followuppro.co.uk:4002/'
+  production: false,
+  url: 'https://www.followuppro.co.uk:3001/',
+  app_version: '100',
+  app_type: '1',
+  device_id: '1',
+  device_type: '0',
+  device_token: 'Website'
 };
